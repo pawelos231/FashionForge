3d76838a188de1b5bb31cf72521db1ddabe49f58
pawelLinek <pawellinek3D@gmail.com>
2023-08-13 15:34:15 +0200

    implemented useLocalStorage

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+type FuncType = <T>(val: T) => any;
+type updateValue<K> = K extends Function ? FuncType : K;
+
+const functionTypeName = "function";
+
+const useLocalStorage = <T>(key: string, value?: T) => {
+  const [storageVal, setStorageVal] = useState<T>(() => {
+    try {
+      const val = localStorage.getItem(key);
+      if (val) {
+        return JSON.parse(val);
+      } else {
+        return value;
+      }
+    } catch (e) {
+      return value;
+    }
+  });
+
+  const updateLocalStorage = <K>(newValue: updateValue<K>) => {
+    if (typeof newValue == functionTypeName) {
+      const fn = newValue as FuncType;
+      setStorageVal(fn(storageVal));
+    } else {
+      const val = newValue as any;
+      setStorageVal(val);
+    }
+  };
+
+  useEffect(() => {
+    const rawValue = JSON.stringify(storageVal);
+    localStorage.setItem(key, rawValue);
+  }, [storageVal]);
+
+  return [storageVal, updateLocalStorage];
+};
+
+export default useLocalStorage;
