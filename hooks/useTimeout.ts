aa4b38ddbc3ea87bf638eba6f92332f0bc5cabde
pawelLinek <pawellinek3D@gmail.com>
2023-08-14 14:45:28 +0200

    useTimeout hook impl

---
+++ hooks/useTimeout.ts
@@ -0,0 +1,34 @@
+import { useRef, useCallback, useEffect } from "react";
+
+const useTimeout = <T extends Function>(callback: T, delay: number) => {
+  const callbackRef = useRef<T>(callback);
+  const clearRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const set = useCallback(() => {
+    clearRef.current = setTimeout(() => callbackRef.current(), delay);
+  }, [delay]);
+
+  const clear = useCallback(() => {
+    if (clearRef.current) {
+      clearTimeout(clearRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    set();
+    return () => clear();
+  }, [delay, set, clear]);
+
+  const reset = useCallback(() => {
+    clear();
+    set();
+  }, [clear, set]);
+
+  return { reset, clear };
+};
+
+export default useTimeout;
